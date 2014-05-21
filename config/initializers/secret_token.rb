8b7e557c3a64fdc8334e6d65d0ff1e7df4c935f6
Thomas Dowling <thomas@thomas-Vostro-2520.(none)>
2014-05-21 10:12:01 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkepticsSpaAngular::Application.config.secret_token = '6340e8b8d1781a1a7330910b3b6ff1f6b9ce95940637dd0bcb52dc65032b2aa6c3dc8f7469c6cb1cfb82fd945b016be500c2fb7ffa918f2d007e4fde00dd6049'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SkepticsSpaAngular::Application.config.session_store :cookie_store, key: '_skeptics_SPA_angular_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SkepticsSpaAngular::Application.config.session_store :active_record_store
