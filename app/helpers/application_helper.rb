be25653cdc1075cc0e6f414846d286f002275e4a
tomgdow <thomasgdowling@gmail.com>
2014-08-19 20:44:42 +0100

    added helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
+
+  def hidden_div_unless(condition, attributes = {}, &block)
+    unless condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
+
+  # Thanks to Agile Web Development by Sam Ruby
+  # 4th Edition (Rails 3.2) (p 145) for the basic hidden_div code
+
 end
